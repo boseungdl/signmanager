5b14a4d7fed67d40f6df3d0119eca4db23b3bfb4
96tmdqh <96tmdqh@naver.com>
2025-06-11 11:56:33 +0900

    Feat : Spotless 적용 (#1)
    - 코드 포맷팅을 위한 Spotless를 프로젝트에 적용함.
    - Windows 환경에서 발생하는 chmod 관련 빌드 오류를 수정하여 호환성을 확보함.

--- src/main/java/com/example/signmanager/SignmanagerApplication.java
+++ src/main/java/com/example/signmanager/SignmanagerApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SignmanagerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SignmanagerApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(SignmanagerApplication.class, args);
+    }
 }
