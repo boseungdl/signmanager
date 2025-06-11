072710680a3aa0253810c866ff99755f6a7b1596
96tmdqh <96tmdqh@naver.com>
2025-06-11 10:39:30 +0900

    init

---
+++ src/main/java/com/example/signmanager/SignmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.signmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SignmanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/signmanager/SignmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.signmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
