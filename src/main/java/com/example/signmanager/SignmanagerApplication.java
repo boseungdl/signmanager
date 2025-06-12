37b025c64d14a6e4ee806e58a008c86200080b64
96tmdqh <96tmdqh@naver.com>
2025-06-12 16:43:34 +0900

    feat: 커밋테스트 (#0)

--- src/main/java/com/example/signmanager/SignmanagerApplication.java
+++ src/main/java/com/example/signmanager/SignmanagerApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SignmanagerApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SignmanagerApplication.class, args);
     }
